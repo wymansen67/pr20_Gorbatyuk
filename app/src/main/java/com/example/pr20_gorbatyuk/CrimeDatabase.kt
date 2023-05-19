ad14f6be2d898f1dc8a1c1f810e8d24653e088b5
wymansen67 <DenisGorbatuk77@gmail.com>
2023-05-19 10:01:00 +0500

    2nd commit

--- app/src/main/java/com/example/pr20_gorbatyuk/CrimeDatabase.kt
+++ app/src/main/java/com/example/pr20_gorbatyuk/CrimeDatabase.kt
@@ -2,7 +2,9 @@ package com.example.pr20_gorbatyuk
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(entities = [ Crime::class ], version=1)
+@TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase : RoomDatabase() {
 }
